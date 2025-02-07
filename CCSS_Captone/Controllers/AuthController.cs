d9db4570e0770063870435da8b5d60095cfcc90d
phamlenhathoang <Nhathoang1470@gmail.com>
2025-02-07 08:57:05 +0700

    nhathoang_add login

---
+++ CCSS_Captone/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using CCSS_Service.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CCSS_Captone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAccountService accountService;
+
+        public AuthController(IAccountService accountService)
+        {
+            this.accountService = accountService;
+        }
+
+        [HttpGet("Login")]
+        public async Task<IActionResult> Login([FromQuery][Required] string email, [FromQuery][Required] string password)
+        {
+            try
+            {
+                var result = await accountService.Login(email, password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ CCSS_Repository/Repositories/AccountRepository.cs
@@ -0,0 +1,29 @@
+﻿using CCSS_Repository.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSS_Repository.Repositories
+{
+
+    public interface IAccountRepository
+    {
+        Task<Account> GetAccount(string email, string password);
+    }
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly CCSSDBContext _dbContext;
+
+        public AccountRepository(CCSSDBContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+        public Task<Account> GetAccount(string email, string password)
+        {
+            return _dbContext.Accounts.Include(a => a.Role).FirstOrDefaultAsync(a => a.Email == email && a.Password == password);
+        }
+    }
+}
