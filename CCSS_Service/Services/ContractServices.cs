45f227d9a68201a410ad0cfd8fa1fe940d620021
Tuấn Anh <146682625+tuananhtuan234@users.noreply.github.com>
2025-02-20 22:40:04 +0700

    tuananh update contract

--- CCSS_Service/Services/ContractServices.cs
+++ CCSS_Service/Services/ContractServices.cs
@@ -1,6 +1,7 @@
 ﻿using CCSS_Repository.Entities;
 using CCSS_Repository.Repositories;
 using CCSS_Service.Model.Requests;
+using CCSS_Service.Model.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,8 @@ namespace CCSS_Service.Services
         Task<List<Contract>> GetAllContract(string searchterm);
         Task<Contract> GetContract(string id);
         Task<string> AddContract(ContractRequest contractRequest);
+        Task<string> UpdateContract(string contractId, ContractResponse contractResponse);
+        Task DeleteContract(string id);
 
     }
     public class ContractServices: IContractServices
@@ -68,6 +71,26 @@ namespace CCSS_Service.Services
             return "Add Success";
         }
 
-        
+        public async Task<string> UpdateContract(string contractId, ContractResponse contractResponse)
+        {
+            var contractExicting = await _respository.GetContractById(contractId);
+            if (contractExicting == null)
+            {
+                return "Contract is not found";
+            }
+            contractExicting.PackageId = contractResponse.PackageId;
+            contractExicting.ContractName = contractResponse.ContractName;
+            contractExicting.Description = contractResponse.Description;
+            contractExicting.Price = contractResponse.Price;
+            contractExicting.Amount = contractResponse.Amount;
+            contractExicting.Signature = contractResponse.Signature;
+            contractExicting.Deposit = contractResponse.Deposit;
+            contractExicting.Status = contractResponse.Status;
+            contractExicting.EndDate = DateTime.Now;
+
+            await _respository.UpdateContract(contractExicting);
+            return "Update Success";
+        }
+      
     }
 }
