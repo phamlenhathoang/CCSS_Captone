993d598b748e7923a56c622356868802c888dd1e
unknown <namthpse161977@fpt.edu.vn>
2025-02-23 23:49:22 +0700

    seed data

---
+++ CCSS_Service/Model/Requests/TicketRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSS_Service.Model.Requests
+{
+    internal class TicketRequest
+    {
+    }
+}
---
+++ CCSS_Service/Model/Responses/EventResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSS_Service.Model.Responses
+{
+    internal class EventResponse
+    {
+    }
+}
