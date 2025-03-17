2dab62aae0bcd7cdc2e44e56a6c76a84208e76b7
unknown <namthpse161977@fpt.edu.vn>
2025-03-17 21:48:49 +0700

    get contractid in payment

--- CCSS_Captone/Controllers/DashboardController.cs
+++ CCSS_Captone/Controllers/DashboardController.cs
@@ -13,52 +13,52 @@ namespace CCSS_Api.Controllers
     [ApiController]
     public class DashBoardController : ControllerBase
     {
-        //private readonly IDashBoardService _dashBoardService;
+        private readonly IDashBoardService _dashBoardService;
 
-        //public DashBoardController(IDashBoardService dashBoardService)
-        //{
-        //    _dashBoardService = dashBoardService;
-        //}
+        public DashBoardController(IDashBoardService dashBoardService)
+        {
+            _dashBoardService = dashBoardService;
+        }
 
-        ///// <summary>
-        ///// Lấy tổng doanh thu theo thời gian (hôm nay, tuần này, tháng này, năm nay).
-        ///// </summary>
-        //[HttpGet("revenue")]
-        //public async Task<ActionResult<DashBoardRevenueResponse>> GetRevenue([FromQuery] DateFilterType filterType)
+        /// <summary>
+        /// Lấy tổng doanh thu theo thời gian (hôm nay, tuần này, tháng này, năm nay).
+        /// </summary>
+        [HttpGet("revenue")]
+        public async Task<ActionResult<DashBoardRevenueResponse>> GetRevenue([FromQuery] DateFilterType filterType)
+        {
+            var result = await _dashBoardService.GetRevenueAsync(filterType);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Lấy danh sách hợp đồng theo trạng thái và thời gian.
+        /// </summary>
+        //[HttpGet("contracts")]
+        //public async Task<ActionResult<List<Contract>>> GetContractsByStatus([FromQuery] ContractStatus status, [FromQuery] DateFilterType filterType)
         //{
-        //    var result = await _dashBoardService.GetRevenueAsync(filterType);
+        //    var result = await _dashBoardService.GetContractsByStatusAsync(status, filterType);
         //    return Ok(result);
         //}
 
-        ///// <summary>
-        ///// Lấy danh sách hợp đồng theo trạng thái và thời gian.
-        ///// </summary>
-        ////[HttpGet("contracts")]
-        ////public async Task<ActionResult<List<Contract>>> GetContractsByStatus([FromQuery] ContractStatus status, [FromQuery] DateFilterType filterType)
-        ////{
-        ////    var result = await _dashBoardService.GetContractsByStatusAsync(status, filterType);
-        ////    return Ok(result);
-        ////}
+        [HttpGet("top-accounts")]
+        public async Task<ActionResult<List<AccountResponse>>> GetTopAccounts()
+        {
+            var result = await _dashBoardService.GetTop5AccountsWithMostPaymentsAsync();
+            return Ok(result);
+        }
+        [HttpGet("top-cosplayer")]
+        public async Task<ActionResult<List<AccountResponse>>> GetTopCosplayer([FromQuery] DateFilterType filterType)
+        {
+            var result = await _dashBoardService.Get5PopularCosplayers(filterType);
+            return Ok(result);
+        }
 
-        //[HttpGet("top-accounts")]
-        //public async Task<ActionResult<List<AccountResponse>>> GetTopAccounts()
-        //{
-        //    var result = await _dashBoardService.GetTop5AccountsWithMostPaymentsAsync();
-        //    return Ok(result);
-        //}
-        //[HttpGet("top-cosplayer")]
-        //public async Task<ActionResult<List<AccountResponse>>> GetTopCosplayer([FromQuery] DateFilterType filterType)
-        //{
-        //    var result = await _dashBoardService.Get5PopularCosplayers(filterType);
-        //    return Ok(result);
-        //}
-        
-        //[HttpGet("total-average-star")]
-        //public async Task<IActionResult> GetAverageStarByContractDescription()
-        //{
-        //    double averageStar = await _dashBoardService.GetAverageStarByContractDescriptionAsync();
+        [HttpGet("total-average-star")]
+        public async Task<IActionResult> GetAverageStarByContractDescription()
+        {
+            double averageStar = await _dashBoardService.GetAverageStarByContractDescriptionAsync();
 
-        //    return Ok(new { AverageStar = averageStar });
-        //}
+            return Ok(new { AverageStar = averageStar });
+        }
     }
 }
--- CCSS_Repository/Repositories/DashBoardRepository.cs
+++ CCSS_Repository/Repositories/DashBoardRepository.cs
@@ -11,133 +11,133 @@ namespace CCSS_Repository.Repositories
 {
     public interface IDashBoardRepository
     {
-        //Task<List<Payment>> GetALlRevenue(DateFilterType filterType);
-        ////Task<List<Contract>> GetContractsByStatusAndDate(ContractStatus status, DateFilterType filterType);
-        //Task<List<Account>> GetTop5AccountsWithMostPaymentsAsync();
-        //Task<List<Feedback>> GetFeedbacksByContractDescriptionAsync();
-        //Task<List<Account>> Get5PopularCosplayers(DateFilterType filterType);
+        Task<List<Payment>> GetALlRevenue(DateFilterType filterType);
+        //Task<List<Contract>> GetContractsByStatusAndDate(ContractStatus status, DateFilterType filterType);
+        Task<List<Account>> GetTop5AccountsWithMostPaymentsAsync();
+        Task<List<Feedback>> GetFeedbacksByContractDescriptionAsync();
+        Task<List<Account>> Get5PopularCosplayers(DateFilterType filterType);
     }
 
         public class DashBoardRepository : IDashBoardRepository
     {
-        //private readonly CCSSDbContext _context;
-        //public DashBoardRepository(CCSSDbContext cCSSDbContext)
-        //{
-        //    _context = cCSSDbContext;
-        //}
-        //public async Task<List<Payment>> GetALlRevenue(DateFilterType filterType)
-        //{
-        //    var now = DateTime.UtcNow;
-        //    var query = _context.Payments
-        //        .Where(p => p.Status == PaymentStatus.Complete && // PaymentStatus = 1
-        //                    (p.Purpose == PaymentPurpose.BuyTicket ||
-        //                     p.Purpose == PaymentPurpose.contractSettlement ||
-        //                     p.Purpose == PaymentPurpose.Order)) // PaymentPurpose = 0, 2, 3
-        //        .AsQueryable();
+        private readonly CCSSDbContext _context;
+        public DashBoardRepository(CCSSDbContext cCSSDbContext)
+        {
+            _context = cCSSDbContext;
+        }
+        public async Task<List<Payment>> GetALlRevenue(DateFilterType filterType)
+        {
+            var now = DateTime.UtcNow;
+            var query = _context.Payments
+                .Where(p => p.Status == PaymentStatus.Complete && // PaymentStatus = 1
+                            (p.Purpose == PaymentPurpose.BuyTicket ||
+                             p.Purpose == PaymentPurpose.contractSettlement ||
+                             p.Purpose == PaymentPurpose.Order)) // PaymentPurpose = 0, 2, 3
+                .AsQueryable();
 
-        //    switch (filterType)
-        //    {
-        //        case DateFilterType.Today:
-        //            query = query.Where(p => p.CreatAt.HasValue &&
-        //                                     p.CreatAt.Value.Date == now.Date);
-        //            break;
+            switch (filterType)
+            {
+                case DateFilterType.Today:
+                    query = query.Where(p => p.CreatAt.HasValue &&
+                                             p.CreatAt.Value.Date == now.Date);
+                    break;
 
-        //        case DateFilterType.ThisWeek:
-        //            var dayOfWeek = (int)now.DayOfWeek;
-        //            var startOfWeek = now.Date.AddDays(dayOfWeek == 0 ? -6 : -dayOfWeek + 1); // Luôn lùi về Thứ Hai tuần hiện tại
-        //            var endOfWeek = startOfWeek.AddDays(6); // Chủ Nhật cùng tuần
-        //            query = query.Where(p => p.CreatAt.HasValue &&
-        //                                     p.CreatAt.Value.Date >= startOfWeek &&
-        //                                     p.CreatAt.Value.Date <= endOfWeek);
-        //            break;
+                case DateFilterType.ThisWeek:
+                    var dayOfWeek = (int)now.DayOfWeek;
+                    var startOfWeek = now.Date.AddDays(dayOfWeek == 0 ? -6 : -dayOfWeek + 1); // Luôn lùi về Thứ Hai tuần hiện tại
+                    var endOfWeek = startOfWeek.AddDays(6); // Chủ Nhật cùng tuần
+                    query = query.Where(p => p.CreatAt.HasValue &&
+                                             p.CreatAt.Value.Date >= startOfWeek &&
+                                             p.CreatAt.Value.Date <= endOfWeek);
+                    break;
 
-        //        case DateFilterType.ThisMonth:
-        //            query = query.Where(p => p.CreatAt.HasValue &&
-        //                                     p.CreatAt.Value.Year == now.Year &&
-        //                                     p.CreatAt.Value.Month == now.Month);
-        //            break;
+                case DateFilterType.ThisMonth:
+                    query = query.Where(p => p.CreatAt.HasValue &&
+                                             p.CreatAt.Value.Year == now.Year &&
+                                             p.CreatAt.Value.Month == now.Month);
+                    break;
 
-        //        case DateFilterType.ThisYear:
-        //            query = query.Where(p => p.CreatAt.HasValue &&
-        //                                     p.CreatAt.Value.Year == now.Year);
-        //            break;
-        //    }
+                case DateFilterType.ThisYear:
+                    query = query.Where(p => p.CreatAt.HasValue &&
+                                             p.CreatAt.Value.Year == now.Year);
+                    break;
+            }
 
-        //    return await query.ToListAsync();
-        //}
+            return await query.ToListAsync();
+        }
 
-        //public async Task<List<Contract>> GetContractsByStatusAndDate(ContractStatus status, DateFilterType filterType)
-        //{
-        //    var now = DateTime.UtcNow;
-        //    var query = _context.Contracts.Where(c => c.Status == status).AsQueryable();
+        public async Task<List<Contract>> GetContractsByStatusAndDate(ContractStatus status, DateFilterType filterType)
+        {
+            var now = DateTime.UtcNow;
+            var query = _context.Contracts.Where(c => c.Status == status).AsQueryable();
 
-        //    switch (filterType)
-        //    {
-        //        case DateFilterType.Today:
-        //            query = query.Where(c => c.StartDate.Date == now.Date);
-        //            break;
+            switch (filterType)
+            {
+                case DateFilterType.Today:
+                    query = query.Where(c => c.StartDate.Date == now.Date);
+                    break;
 
-        //        case DateFilterType.ThisWeek:
-        //            var dayOfWeek = (int)now.DayOfWeek;
-        //            var startOfWeek = now.Date.AddDays(dayOfWeek == 0 ? -6 : -(dayOfWeek - 1)); // Nếu Chủ nhật (0), lùi về Thứ 2 tuần trước
-        //            query = query.Where(c => c.StartDate.Date >= startOfWeek && c.StartDate.Date <= now.Date);
-        //            break;
+                case DateFilterType.ThisWeek:
+                    var dayOfWeek = (int)now.DayOfWeek;
+                    var startOfWeek = now.Date.AddDays(dayOfWeek == 0 ? -6 : -(dayOfWeek - 1)); // Nếu Chủ nhật (0), lùi về Thứ 2 tuần trước
+                    query = query.Where(c => c.StartDate.Date >= startOfWeek && c.StartDate.Date <= now.Date);
+                    break;
 
-        //        case DateFilterType.ThisMonth:
-        //            query = query.Where(c => c.StartDate.Year == now.Year && c.StartDate.Month == now.Month);
-        //            break;
+                case DateFilterType.ThisMonth:
+                    query = query.Where(c => c.StartDate.Year == now.Year && c.StartDate.Month == now.Month);
+                    break;
 
-        //        case DateFilterType.ThisYear:
-        //            query = query.Where(c => c.StartDate.Year == now.Year);
-        //            break;
-        //    }
+                case DateFilterType.ThisYear:
+                    query = query.Where(c => c.StartDate.Year == now.Year);
+                    break;
+            }
 
-        //    return await query.ToListAsync();
-        //}
-        //public async Task<List<Account>> GetTop5AccountsWithMostPaymentsAsync()
-        //{
-        //    return await _context.Accounts
-        //        .Where(a => a.Contracts.Any(c => c.Payments.Any(p => (int?)p.Status == 1 && (int?)p.Purpose == 2))) 
-        //        .Include(a => a.Contracts)
-        //        .ThenInclude(c => c.Payments)
-        //        .OrderByDescending(a => a.Contracts.SelectMany(c => c.Payments).Count(p => (int?)p.Status == 1 && (int?)p.Purpose == 2)) 
-        //        .Take(5)
-        //        .ToListAsync();
-        //}
+            return await query.ToListAsync();
+        }
+        public async Task<List<Account>> GetTop5AccountsWithMostPaymentsAsync()
+        {
+            return await _context.Accounts
+                .Where(a => a.Contracts.Any(c => c.Payments.Any(p => (int?)p.Status == 1 && (int?)p.Purpose == 2)))
+                .Include(a => a.Contracts)
+                .ThenInclude(c => c.Payments)
+                .OrderByDescending(a => a.Contracts.SelectMany(c => c.Payments).Count(p => (int?)p.Status == 1 && (int?)p.Purpose == 2))
+                .Take(5)
+                .ToListAsync();
+        }
 
-        //public async Task<List<Feedback>> GetFeedbacksByContractDescriptionAsync()
-        //{
-        //    return await _context.Feedbacks
-        //        .Where(f => f.Contract != null && (int?)f.Contract.Description == 1)
-        //        .ToListAsync();
-        //}
-        //public async Task<List<Account>> Get5PopularCosplayers(DateFilterType filterType)
-        //{
-        //    DateTime startDate = filterType switch
-        //    {
-        //        DateFilterType.Today => DateTime.UtcNow.Date,
-        //        DateFilterType.ThisWeek => DateTime.UtcNow.AddDays(-(int)DateTime.UtcNow.DayOfWeek + 1).Date, // Đầu tuần (Thứ Hai)
-        //        DateFilterType.ThisMonth => new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, 1), // Đầu tháng
-        //        DateFilterType.ThisYear => new DateTime(DateTime.UtcNow.Year, 1, 1), // Đầu năm
-        //        _ => DateTime.UtcNow.Date
-        //    };
+        public async Task<List<Feedback>> GetFeedbacksByContractDescriptionAsync()
+        {
+            return await _context.Feedbacks
+                .Where(f => f.Contract != null && (int?)f.Contract.Description == 1)
+                .ToListAsync();
+        }
+        public async Task<List<Account>> Get5PopularCosplayers(DateFilterType filterType)
+        {
+            DateTime startDate = filterType switch
+            {
+                DateFilterType.Today => DateTime.UtcNow.Date,
+                DateFilterType.ThisWeek => DateTime.UtcNow.AddDays(-(int)DateTime.UtcNow.DayOfWeek + 1).Date, // Đầu tuần (Thứ Hai)
+                DateFilterType.ThisMonth => new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, 1), // Đầu tháng
+                DateFilterType.ThisYear => new DateTime(DateTime.UtcNow.Year, 1, 1), // Đầu năm
+                _ => DateTime.UtcNow.Date
+            };
 
-        //    return await _context.Accounts
-        //        .Where(a => a.Role.RoleName == RoleName.Cosplayer &&
-        //            a.ContractCharacter != null && // Kiểm tra có ContractCharacter không
-        //            a.ContractCharacter.Contract.Status == ContractStatus.Completed &&
-        //            a.ContractCharacter.Contract.Description == ContractDescription.RentCosplayer &&
-        //            a.ContractCharacter.Contract.Payments.Any(p => p.Status == PaymentStatus.Complete &&
-        //                                                           p.Purpose == PaymentPurpose.contractSettlement &&
-        //                                                           p.CreatAt.HasValue && p.CreatAt.Value >= startDate))
-        //        .OrderByDescending(a => a.Contracts.Count(c => c.Status == ContractStatus.Completed &&
-        //                                                       c.Description == ContractDescription.RentCosplayer &&
-        //                                                       c.Payments.Any(p => p.Status == PaymentStatus.Complete &&
-        //                                                                           p.Purpose == PaymentPurpose.contractSettlement &&
-        //                                                                           p.CreatAt.HasValue && p.CreatAt.Value >= startDate)))
-        //        .Take(5)
-        //        .ToListAsync();
-        //}
+            return await _context.Accounts
+                .Where(a => a.Role.RoleName == RoleName.Cosplayer &&
+                    a.ContractCharacter != null && // Kiểm tra có ContractCharacter không
+                    a.ContractCharacter.Contract.Status == ContractStatus.Completed &&
+                    a.ContractCharacter.Contract.Description == ContractDescription.RentCosplayer &&
+                    a.ContractCharacter.Contract.Payments.Any(p => p.Status == PaymentStatus.Complete &&
+                                                                   p.Purpose == PaymentPurpose.contractSettlement &&
+                                                                   p.CreatAt.HasValue && p.CreatAt.Value >= startDate))
+                .OrderByDescending(a => a.Contracts.Count(c => c.Status == ContractStatus.Completed &&
+                                                               c.Description == ContractDescription.RentCosplayer &&
+                                                               c.Payments.Any(p => p.Status == PaymentStatus.Complete &&
+                                                                                   p.Purpose == PaymentPurpose.contractSettlement &&
+                                                                                   p.CreatAt.HasValue && p.CreatAt.Value >= startDate)))
+                .Take(5)
+                .ToListAsync();
+        }
 
 
 
