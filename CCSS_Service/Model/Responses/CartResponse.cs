39f6e1d03387de09e9ffb0e20d2b6ea710592fcd
Tuấn Anh <146682625+tuananhtuan234@users.noreply.github.com>
2025-03-25 23:09:32 +0700

    tuananh cart response

---
+++ CCSS_Service/Model/Responses/CartResponse.cs
@@ -0,0 +1,30 @@
+﻿using CCSS_Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSS_Service.Model.Responses
+{
+    public class CartResponse
+    {
+        public string CartId { get; set; }
+        public string AccountId { get; set; }
+        public double TotalPrice { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+
+        public List<CartProductResponse> ListCartProduct { get; set; }
+    }
+
+    public class CartProductResponse
+    {
+        public string CartProductId { get; set; } 
+        public string ProductId { get; set; }
+        public double? Price { get; set; }
+        public int? Quantity { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
--- CCSS_Service/Services/CartServices.cs
+++ CCSS_Service/Services/CartServices.cs
@@ -1,41 +1,95 @@
 ﻿using CCSS_Repository.Entities;
 using CCSS_Repository.Repositories;
 using CCSS_Service.Model.Requests;
+using CCSS_Service.Model.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static iText.StyledXmlParser.Jsoup.Select.Evaluator;
 using Task = System.Threading.Tasks.Task;
 
 namespace CCSS_Service.Services
 {
     public interface ICartServices
     {
-        Task<Cart> GetCartById(string id);
-        Task<Cart> GetCartByAccount(string accountId);
+        Task<CartResponse> GetCartById(string id);
+        Task<CartResponse> GetCartByAccount(string accountId);
         Task<string> AddCart(CartRequest cartRequest);
     }
 
-    public class CartServices: ICartServices
+    public class CartServices : ICartServices
     {
         private readonly ICartRepository _cartRepository;
         private readonly IAccountRepository _accountRepository;
+        private readonly ICartProductRepository _cartProductRepository;
+        private readonly IProductRepository _productRepository;
 
-        public CartServices(ICartRepository cartRepository, IAccountRepository accountRepository)
+        public CartServices(ICartRepository cartRepository, ICartProductRepository cartProductRepository, IAccountRepository accountRepository, IProductRepository productRepository)
         {
             _accountRepository = accountRepository;
             _cartRepository = cartRepository;
+            _productRepository = productRepository;
+            _cartProductRepository = cartProductRepository;
         }
 
-        public async Task<Cart> GetCartById(string id)
+        public async Task<CartResponse> GetCartById(string id)
         {
-            return await _cartRepository.GetCartById(id);
+            var cart = await _cartRepository.GetCartById(id);
+            var listCartProduct = await _cartProductRepository.GetListProductInCart(id);
+            CartResponse cartResponse = new CartResponse()
+            {
+                CartId = cart.CartId,
+                AccountId = cart.AccountId,
+                TotalPrice = cart.TotalPrice,
+                CreateDate = cart.CreateDate,
+                UpdateDate = cart.UpdateDate,
+                ListCartProduct = listCartProduct.Select(cp => new CartProductResponse
+                {
+                    CartProductId = cp.ProductId,
+                    ProductId = cp.ProductId,
+                    Price = cp.Price,
+                    Quantity = cp.Quantity,
+                    CreatedDate = cp.CreatedDate,
+                }).ToList(),
+            };
+            return cartResponse;
+
         }
 
-        public async Task<Cart> GetCartByAccount(string accountId)
+        public async Task<CartResponse> GetCartByAccount(string accountId)
         {
-            return await _cartRepository.GetcartByAccount(accountId);
+            try
+            {
+                var cart = await _cartRepository.GetcartByAccount(accountId);
+                if (cart == null)
+                {
+                    throw new Exception("Cart not found");
+                }
+                var listCartProduct = await _cartProductRepository.GetListProductInCart(cart.CartId);
+                CartResponse cartResponse = new CartResponse()
+                {
+                    CartId = cart.CartId,
+                    AccountId = cart.AccountId,
+                    TotalPrice = cart.TotalPrice,
+                    CreateDate = cart.CreateDate,
+                    UpdateDate = cart.UpdateDate,
+                    ListCartProduct = listCartProduct.Select(cp => new CartProductResponse
+                    {
+                        CartProductId = cp.ProductId,
+                        ProductId = cp.ProductId,
+                        Price = cp.Price,
+                        Quantity = cp.Quantity,
+                        CreatedDate = cp.CreatedDate,
+                    }).ToList(),
+                };
+                return cartResponse;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public async Task<string> AddCart(CartRequest cartRequest)
@@ -45,11 +99,11 @@ namespace CCSS_Service.Services
             {
                 return "Account not found";
             }
-            if(account.RoleId != "R005")
+            if (account.RoleId != "R005")
             {
                 return "You must be a customer ";
             }
-            if(cartRequest == null)
+            if (cartRequest == null)
             {
                 return "Please entry field";
             }
@@ -62,7 +116,7 @@ namespace CCSS_Service.Services
                 UpdateDate = null,
             };
 
-           var result = await _cartRepository.AddCart(cart);
+            var result = await _cartRepository.AddCart(cart);
             return result ? "Add Success" : "Add Failed";
         }
     }
