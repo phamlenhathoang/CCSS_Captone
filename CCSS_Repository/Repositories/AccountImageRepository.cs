dc4a81d72ac29207c05a38739099d78e2523c4c5
phamlenhathoang <Nhathoang1470@gmail.com>
2025-03-29 16:42:49 +0700

    add get accountimage by id and accountId

--- CCSS_Repository/Repositories/AccountImageRepository.cs
+++ CCSS_Repository/Repositories/AccountImageRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CCSS_Repository.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,8 @@ namespace CCSS_Repository.Repositories
     public interface IAccountImageRepository
     {
         Task<bool> AddListAccountImage (List<AccountImage> accountImages);
+        Task<AccountImage> GetAccountImageById (string id);
+        Task<List<AccountImage>> GetListAccountImagesByAccountId(string id);
     }
     public class AccountImageRepository : IAccountImageRepository
     {
@@ -23,6 +26,16 @@ namespace CCSS_Repository.Repositories
         {
             await _dbContext.AddRangeAsync(accountImages);  
             return await _dbContext.SaveChangesAsync() > 0 ? true : false;
+        }
+
+        public async Task<AccountImage> GetAccountImageById(string id)
+        {
+            return await _dbContext.AccountImages.FirstOrDefaultAsync(ac => ac.AccountImageId.Equals(id));
+        }
+
+        public async Task<List<AccountImage>> GetListAccountImagesByAccountId(string id)
+        {
+            return await _dbContext.AccountImages.Include(a => a.Account).Where(ac => ac.AccountId.Equals(id)).ToListAsync();
         }
     }
 }
