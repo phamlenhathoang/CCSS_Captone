d54380e18b31d4d0470bf84a184a0cfa9478fb50
Tuấn Anh <146682625+tuananhtuan234@users.noreply.github.com>
2025-04-09 06:38:28 +0700

    tuananh fix package

--- CCSS_Service/Model/Requests/PackageRequest.cs
+++ CCSS_Service/Model/Requests/PackageRequest.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CCSS_Service.Model.Responses
+namespace CCSS_Service.Model.Requests
 {
-    public class PackageResponse
+    public class PackageRequest
     {
-        public string PackageId { get; set; }
         public string PackageName { get; set; }
         public string? Description { get; set; }
         public double? Price { get; set; }
