05cbccaf4852a8bef17d627f2d7b6d63d23d2176
phamlenhathoang <Nhathoang1470@gmail.com>
2025-04-16 22:02:04 +0700

    Add GetContractCharactersByContractId(string contractId)

---
+++ CCSS_Captone/Controllers/ContractCharacterController.cs
@@ -0,0 +1,25 @@
+﻿using CCSS_Service.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCSS_Captone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractCharacterController : ControllerBase
+    {
+        private readonly IContractCharacterService contractCharacterService;
+
+        public ContractCharacterController(IContractCharacterService contractCharacterService)
+        {
+            this.contractCharacterService = contractCharacterService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetContractCharactersByContractId(string contractId)
+        {
+            var contractCharacters = await contractCharacterService.GetContractCharactersByContractId(contractId);
+            return Ok(contractCharacters);
+        }
+    }
+}
