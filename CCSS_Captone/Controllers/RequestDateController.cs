efe7acc1649aefd63c2fcb534c5368d407b4d64f
Tuấn Anh <146682625+tuananhtuan234@users.noreply.github.com>
2025-04-18 00:54:11 +0700

    tuananh create request date controller

---
+++ CCSS_Captone/Controllers/RequestDateController.cs
@@ -0,0 +1,52 @@
+﻿using CCSS_Service.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCSS_Captone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestDateController : ControllerBase
+    {
+        private readonly IRequestDateServices _services;
+
+        public RequestDateController(IRequestDateServices services)
+        {
+            _services = services;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRequestDateById(string id)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _services.GetRequestDate(id);
+                return Ok(result);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet("CalculateTotalHours")]
+        public async Task<IActionResult> CalculateTotalHours(string requestcharacterId)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _services.CaculateTotalHour(requestcharacterId);
+                return Ok(result);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet("ListRequestDateByRequestChracter")]
+        public async Task<IActionResult> GetListRequestDateByRequestChracterId(string requestCharacterId)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _services.GetListRequestDateByRequestCharacterId(requestCharacterId);
+                return Ok(result);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
