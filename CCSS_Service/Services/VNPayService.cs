204714389bab589d0b87c75dd3e12a77b03c9eb0
unknown <namthpse161977@fpt.edu.vn>
2025-05-19 17:15:22 +0700

    send mail

--- CCSS_Service/Services/VNPayService.cs
+++ CCSS_Service/Services/VNPayService.cs
@@ -274,7 +274,7 @@ namespace CCSS_Service.Services
 
                     Contract contract1 = await contractRespository.GetContractById(existingPayment.ContractId);
                     var customer1 = await _accountRepository.GetAccountByAccountId(response.AccountId);
-                    bool rs = await _contractServices.UpdateStatusContract(contract1.ContractId, "Completed", response.Amount, null);
+                    bool rs = await _contractServices.UpdateStatusContract(contract1.ContractId, "Completed", contract1.Amount, null);
                     if (!rs)
                     {
                         throw new Exception("Can not update status contract");
